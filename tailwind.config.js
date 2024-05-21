21ab55e01f26107c54e08bcae99b49ff22f028d9
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-05-21 23:29:20 +0600

    all done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        mxxxl: "1700px",
+        mxxl: "1500px",
+        ml: "425px",
+        mm: "375px",
+        mv: "550px"
+      }
+    },
+  },
+  plugins: [],
+}
+
